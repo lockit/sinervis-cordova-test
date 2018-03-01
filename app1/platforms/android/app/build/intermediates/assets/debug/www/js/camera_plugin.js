938a24cbc4970a03ea1a92fde013be79e3b28be9
Alessandro <lockit@lockit.it>
2018-03-01 15:38:03 +0100

    gallery

--- app1/platforms/android/app/build/intermediates/assets/debug/www/js/camera_plugin.js
+++ app1/platforms/android/app/build/intermediates/assets/debug/www/js/camera_plugin.js
@@ -1,18 +1,57 @@
 var camera_plugin = {
   config: {
-    elementId: '#myImage'
+    elementId: '#myImage',
+    galleryFolder: 'myPhoto'
   },
   capturePhoto: function(){
     navigator.camera.getPicture(this.onPhotoTaken, this.onPhotoFail, {
       quality: 50,
-      destinationType: Camera.DestinationType.FILE_URI
+      destinationType: Camera.DestinationType.FILE_URI,
+      correctOrientation: true
     });
   },
   onPhotoTaken: function(imageURI){
     var myImage = $(camera_plugin.config.elementId);
     myImage.attr('src', imageURI);
+    app.refreshGallery();
+    camera_plugin.movePhoto(imageURI);
   },
   onPhotoFail: function(message){
     console.log('Fallito perché: '+ message);
+  },
+  movePhoto: function(file_uri){
+    window.resolveLocalFileSystemURL(file_uri,
+                                     camera_plugin.onResolveSuccess,
+                                     camera_plugin.onResolveError);
+  },
+  onResolveSuccess: function(entry){
+    var now = new Date();
+    var newName = now.getTime() + '.jpg';
+    var folder = camera_plugin.config.galleryFolder;
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT,
+                             0,
+                             function(fileSys){
+                               fileSys.root.getDirectory(
+                                 folder,
+                                 { create: true, exclusive: false },
+                                 function(directory){
+                                   entry.moveTo(directory,
+                                                newName,
+                                                camera_plugin.onMoveSuccess,
+                                                camera_plugin.onResolveError
+                                              );
+                                 },
+                                 camera_plugin.onResolveError
+                               );
+                             },
+                             camera_plugin.onResolveError
+                          );
+  },
+  onMoveSuccess: function(entry){
+    console.log(entry);
+  },
+  onResolveError: function(error){
+    console.log(error);
   }
 };
--- app1/www/js/index.js
+++ app1/www/js/index.js
@@ -1,4 +1,5 @@
 var app = {
+    galleryFolder: null,
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
@@ -10,6 +11,25 @@ var app = {
         $('#cameraButton').on('click', function(){
           camera_plugin.capturePhoto();
         });
+        app.galleryFolder = cordova.file.dataDirectory+ 'files/' +
+                            camera_plugin.config.galleryFolder;
+        app.refreshGallery();
+    },
+    refreshGallery: function(){
+      window.resolveLocalFileSystemURL(app.galleryFolder, function(entry){
+        entry.createReader().readEntries(app.loadGallery, app.errorGallery);
+      });
+    },
+    loadGallery: function(entries){
+      var gallery = $("#myGallery");
+      gallery.html('');
+      for(var i=0; i<entries.length; i++){
+        var img = '<div><img src="'+entries[i].nativeURL+'" /></div>';
+        gallery.append(img);
+      }
+    },
+    errorGallery: function(error){
+      console.log(error);
     }
 };
 
